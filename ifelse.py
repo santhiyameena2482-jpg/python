a814c689ea172cc3d1b5c59409a4a6e816dbee15
Santhiya <santhiyameena2482@gmail.com>
2026-06-02 20:57:45 +0530

    if-else
    Signed-off-by: Santhiya <santhiyameena2482@gmail.com>

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+# syntax:it always provide output as boolean value 
+# if(True):                                   if(false):
+#     print( " true value")                      print("false value")
+# else:                                       else:
+#     print("value is not matched to true")      print("value is not matched to false")
+rcb="lose"
+if("win"==rcb):
+    print("Ee sala cup namde")
+else:
+    print("valkampola cup nammaku illa")
+    

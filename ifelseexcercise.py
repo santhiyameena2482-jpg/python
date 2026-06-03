3ec5a76d62e715cd0d7c234a761968d999ee2f8a
Santhiya <santhiyameena2482@gmail.com>
2026-06-03 22:30:14 +0530

    if else-excercise(03/06/2026)
    Signed-off-by: Santhiya <santhiyameena2482@gmail.com>

---
+++ ifelseexcercise.py
@@ -0,0 +1,26 @@
+# Get the input variable as mark.If mark>35 print pass.Else print fail 
+# Sample inputnark:34
+# Sample Output:
+# "fail"
+# mark=int(input())
+# if(mark>35):
+#     print("pass")
+# else:
+#     print("fail")
+
+ # Get input for variable income.If income is greater 
+# than 7000 scholarship is available.Else not eligible for scholarship
+# Sample input 
+# Income:7200
+# Sample Output
+# "Not Eligible for scholarship"
+# income=int(input())
+# if (income>7000):
+#    print("Scholarship is available")
+# else:
+#     print("Not Eligible for scholarship")
+number=int(input())
+if(number/3==number/5):
+    print("Number is divisible by both 3 and 5")
+else:
+    print("Number is not divisible by both 3 and 5")

c0b569d662ae7b7e14be1abc17c2113da1ef4a74
Santhiya <santhiyameena2482@gmail.com>
2026-06-10 22:08:54 +0530

    for with list (08/06/26)
    Signed-off-by: Santhiya <santhiyameena2482@gmail.com>

---
+++ forlist.py
@@ -0,0 +1,26 @@
+# Write a program to compute the sum of the first 5 
+# natural numbers
+# a1=[]
+# sum1=0
+# for i1 in range(1,6):
+#     sum1=sum1+i1
+# print(sum1)
+
+# write a program to read 10 numbers from the keyboard
+# and find their sum and average. 
+# a=[]
+# for i in range(10):
+#     num=int(input())
+#     a.append(num)
+# print(a)
+# sum=0
+# for i in a:
+#     sum=sum+i/10
+# print(sum)
+
+# Write a program to display n terms of natural numbers and
+# their sum 
+# Test Data:7
+# Expected Ouput:
+# The first 7 natural number is:
+#  1 2 3 4 5 6 7

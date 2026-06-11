eb3188ec269d68c11ab04cc515f56073fa4775b9
Santhiya <santhiyameena2482@gmail.com>
2026-06-11 20:05:39 +0530

    while loop (11/06/26)
    Signed-off-by: Santhiya <santhiyameena2482@gmail.com>

---
+++ whileloop.py
@@ -0,0 +1,21 @@
+# write a loop statement print the following series
+# 10,20,30,40,50,.......,200
+
+i1=10
+while(i1<=200):
+    print(i1,end=",")
+    i1=i1+10
+
+# write a program print 10 natural numbers in reverse order
+i2=10
+while(i2>0):
+    print(i2,end="'")
+    i2=i2-1
+
+# write a program to find a factorial of number
+i3=3
+fact=1
+while(i3>0):
+    fact=fact*i3
+    i3=i3-1
+print(fact)
